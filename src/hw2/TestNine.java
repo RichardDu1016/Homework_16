e00127d6096cb2accc33155bdc3814993c2c8b93
Richard Du <cloudpb11203@gmail.com>
2023-09-04 16:58:11 +0800

    add TestNine.java

---
+++ src/hw2/TestNine.java
@@ -0,0 +1,50 @@
+package xxx;
+
+public class TestNine {
+	public static void main(String[] args) {
+		
+//		for + while 迴圈
+
+//		
+//		for(int i=1; i<=9; i++) {
+//			int j = 1;
+//			while( j <=9 ) {
+//		
+//				System.out.print(i + "*" + j + "=" + i*j + "\t");
+//				j++;
+//			}
+//			System.out.println();
+//		}
+//		
+		
+//		for + do...while 迴圈
+//		
+//		for(int i=1; i<=9; i++) {
+//			int j = 1;
+//			do{
+//				System.out.print(i + "*" + j + "=" + i*j + "\t");
+//				j++;
+//			} while( j <= 9);
+//			System.out.println();
+//		}
+		
+		
+//		while + do...while 迴圈
+//		int i = 1;
+//		while(i <= 9) {
+//			int j=1;
+//			do {;
+//				System.out.print(i + "*" + j + "=" + i*j + "\t");
+//				j++;
+//			}while(j <= 9);
+//			
+//			i++;
+//			
+//			System.out.println();
+//		}
+		
+	
+
+	}
+}
+
