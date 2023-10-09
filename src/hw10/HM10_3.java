46309f09f8f73ceba81d4884f84262f8c6d42bed
Richard Du <cloudpb11203@gmail.com>
2023-10-09 18:33:57 +0800

    upload homework 10

--- src/hw10/HM10_3.java
+++ src/hw10/HM10_3.java
@@ -2,6 +2,8 @@ package hw10;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HM10_3 {
 	public static void main(String[] args) {
@@ -10,27 +12,38 @@ public class HM10_3 {
 		 String date = sc.next();
 		 
 	
-		 
-		 System.out.println("(1)年/月/日 (2)月/日/年 (3)日/月/年");
-		 int ch = sc.nextInt();
-		 switch(ch) {
-		 case 1 :
-			DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyyMMdd");
-			System.out.println("年/月/日："+ LocalDate.parse(date,formatter1));	
-			break;
-		case 2 :
-			DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MMddyyyy");
-			System.out.println("月/日/年："+ LocalDate.parse(date,formatter2));
-			break;
-		case 3 :
-			DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("ddMMyyyy");
-			System.out.println("年/月/日："+ LocalDate.parse(date,formatter3));
-			break;
-		default:
-			System.out.println("請輸入正確選項");
+		 if(isVailDate(date)) {
+			 System.out.println("(1)年/月/日 (2)月/日/年 (3)日/月/年");
+			 int ch = sc.nextInt();
+			 switch(ch) {
+			 case 1 :
+				DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyyMMdd");
+				System.out.println("年/月/日："+ LocalDate.parse(date,formatter1));	
+				break;
+			case 2 :
+				DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MMddyyyy");
+				System.out.println("月/日/年："+ LocalDate.parse(date,formatter2));
+				break;
+			case 3 :
+				DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("ddMMyyyy");
+				System.out.println("年/月/日："+ LocalDate.parse(date,formatter3));
+				break;
+			default:
+				System.out.println("請輸入正確選項");
+			 }
+		 }else {
+			 System.out.println("請輸入有效的日期格式(yyyyMMdd)的格式");
 		 }
 		
+		
 		 sc.close();
+	}
+	
+	public static boolean isVailDate(String date) {
+		String datepattern = "\\d{8}";
+		Pattern pattern = Pattern.compile(datepattern);
+		Matcher matcher = pattern.matcher(date);
+		return matcher.matches();
 	}
 	}
 
--- src/hw10/HW10_2.java
+++ src/hw10/HW10_2.java
@@ -8,26 +8,34 @@ import java.util.Scanner;
 
 public class HW10_2 {
 	public static void main(String[] args) {
+		
 		System.out.println("請輸入數字");
-		 Scanner sc = new Scanner(System.in);
-		 double  num = sc.nextDouble();
-		 
-		 System.out.println("欲格式化成(1)千分位(2)百分比(3)科學記號");
-		 int ch = sc.nextInt();
-		 switch(ch) {
-		 case 1 :
-			 Format dfm1 = new DecimalFormat("#,###.00");
-			 out.println(dfm1.format(num));
-			 break;
-		case 2 :
-			 Format dfm2 = new DecimalFormat("#,###%");
-			 out.println(dfm2.format(num));
-			 break;
-		case 3 :
-			 Format dfm3 = new DecimalFormat("#.#E00");
-			 out.println(dfm3.format(num));
-			 break;
-		 }
+		Scanner sc = new Scanner(System.in);
+		
+		if(sc.hasNextDouble()) {
+			String  num = sc.next();
+			 
+			System.out.println("欲格式化成(1)千分位(2)百分比(3)科學記號");
+			 int ch = sc.nextInt();
+			 switch(ch) {
+			 case 1 :
+				 Format dfm1 = new DecimalFormat("#,###.00");
+				 out.println(dfm1.format(num));
+				 break;
+			case 2 :
+				 Format dfm2 = new DecimalFormat("#,###%");
+				 out.println(dfm2.format(num));
+				 break;
+			case 3 :
+				 Format dfm3 = new DecimalFormat("#.#E00");
+				 out.println(dfm3.format(num));
+				 break;
+			default:
+				System.out.println("請輸入正確選項!");
+			 }
+		}else {
+			System.out.println("請輸入有效數字");
+		}
 		 sc.close();
 	}
 	
